674ca7f6b894951e58a7df866d72ffc3fd094f6e
henginthere <hypae@naver.com>
2022-01-17 23:27:17 +0900

    'homework01'

--- HWJava01_GUMI06_07th_[배혜연]/src/com/java/first/CircleArea.java
+++ HWJava01_GUMI06_07th_[배혜연]/src/com/java/first/CircleArea.java
@@ -8,6 +8,8 @@ public class CircleArea {
 		System.out.print("원의 반지름 : ");
 		int r = sc.nextInt();
 		double r2 = r*r*3.14;
-		System.out.println("반지름이 "+r+"cm인 원의 넓이는 "+r2+"cm2입니다.");
+		System.out.println("반지름이 "+r+"cm인 원의 넓이는 "+r2+"㎠입니다.");
+		sc.close();
 }
+	
 }
