c26e610140e3abfdbb61038da95e49dbd44e79b1
henginthere <hypae@naver.com>
2022-01-20 00:19:26 +0900

    homework03

--- WSJava03_GUMI06_07th_배혜연/src/com/ssafy/ws07/step3/BookManagerImpl.java
+++ WSJava03_GUMI06_07th_배혜연/src/com/ssafy/ws07/step3/BookManagerImpl.java
@@ -1,140 +1,137 @@
 package com.ssafy.ws07.step3;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import com.ssafy.ws07.step3.Book;
+import com.ssafy.ws07.step3.Magazine;
+import com.ssafy.ws07.step3.ISBNNotFoundException;
 
 public class BookManagerImpl implements IBookManager{
-//	//2. 생성한 객체를 저장할 instance private field 선언
-//	private BookManagerImpl instance; 
-//	//1. 외부에서 객체 생성을 못하도록 막아야 된다.(생성자)
-//	private BookManagerImpl(){ //private 접근제한
-//	}
-//	//3. 외부에서 IBookManagerImpl가 가지고 있는 매니저 객체를 리턴해줄 메서드 작성
-//	public static BookManagerImpl getInstance() {
-//		return new BookManagerImpl();
-//	}
+
 	
-	Book book = new Book();
-	private ArrayList<Book> books = new ArrayList<Book>();
-	Scanner sc = new Scanner(System.in);
+	private static BookManagerImpl instance;
 	
-	public void add(Book book) {
-		int cnt=0;
-		System.out.println("도서등록");
-		System.out.print("등록할 도서의 고유번호를 입력하세요.");
-		String tmp = sc.next();
-		
-		for(int i=0;i<books.size();i++) {
-			if(tmp.equals(books.get(i).getIsbn())){
-				cnt++;
-				System.out.println("이미 등록된 도서입니다.");
-				break;
-			}
+	private BookManagerImpl() {}
+	
+	public static BookManagerImpl getInstance() {
+		if(instance==null) {
+			instance = new BookManagerImpl();
 		}
-		if(cnt==0) {
-			book.setIsbn(tmp);
-		}
-		
-		System.out.println("등록가능.");
-		System.out.println("등록할 도서의 제목, 작가, 출판사, 가격, 간단설명을 입력하세요.");
-		System.out.print("제목 : ");
-		book.setTitle(sc.next());
-		System.out.print("작가 : ");
-		book.setAuthor(sc.next());
-		System.out.print("출판사 : ");
-		book.setPublisher(sc.next());
-		System.out.print("가격 : ");
-		book.setPrice(sc.nextInt());
-		System.out.print("간단설명 : ");
-		book.setDesc(sc.next());
-		
-		books.add(book);
-		System.out.println("등록완료.");
+		return instance;
 	}
+	//int MAX_SIZE=100; //도서정보는 최대 100권을 넘지 않는다.
+	//int size=0;
+	
+	//Book[] books = new Book[MAX_SIZE];
+	private ArrayList<Book> books = new ArrayList<>();
+
+	@Override
+	public void add(Book book) {
+		books.add(book);
+	}
+	
 	public void remove(String isbn) {
 		
-		System.out.println("도서삭제");
-		System.out.print("삭제할 도서의 고유번호를 입력하세요: ");
-		String tmp=sc.next();
-		
-		int cnt = 0;
-        for (int i = 0; i < books.size(); i++) {
-            if (tmp.equals(books.get(i).getIsbn())) {
-                books.remove(i);
-                cnt++;
-                System.out.println("도서 삭제 완료");
-                break;
-            }
-        } 
-        if (cnt == 0) {
-            System.out.println("해당 도서가 존재하지 않습니다.");
-        } 
-	}
-	public Book[] getList() {
-		Book[] temp = new Book[100];
-		int i = 0;
-		for(Book b : books) {
-			if(b == null) {
-				continue;
+		for(Book b:books) {
+			if(b.getIsbn().equals(isbn)) {
+				books.remove(b);
 			}
-			else if(b.equals(books.get(i))) {
-				temp[i] = b;
-			}
-			i++;
 		}
-		return temp;
 	}
+	
+	public ArrayList<Book> getList() {
+	
+		return books;
+	}
+	
 	public Book searchByIsbn(String isbn) {
-		Book temp = null;
+		
+		Book temp = new Book();
 		for(Book b : books) {
-			if(b != null) {
-				if(b.getIsbn().equals(isbn)) {
-					temp = b;
-				}
+			if(b.getIsbn().equals(isbn)) {
+				temp = b;
 			}
 		}
 		return temp;
 	}
-	public Book[] searchByTitle(String title) {
-		Book[] temp = new Book[100];
-		int i=0;
-		int cnt=0;
+
+	
+	public ArrayList<Book> searchByTitle(String title) {
+		
+		ArrayList<Book> temp = new ArrayList<>();
 		for(Book b : books) {
-			if(b != null) {
-				if(b.getTitle().contains(title)) {
-					temp[i++] = b;
-					cnt++;
-				}
+			if(b.getTitle().contains(title)) {
+				temp.add(b);
 			}
 		}
-		Book[] fin = new Book[cnt];
-		for(int j=0;j<cnt;j++) {
-			if(temp[j]!=null) {
-				fin[j]=temp[j];
-			}
-		}
-		return fin;
+		return temp;
 	}
-//	public Magazine[] getMagazines() {
-//		Magazine[] temp = new Magazine[100];
-//		int i = 0;
-//		for(Book b : books) {
-//			if(b == null) {
-//				continue;
-//			}
-//			else if(b.getYear()) {
-//				temp[i] = b;
-//			}
-//			i++;
-//		}
-//		return temp;
-//	}
-//	Book[] getBooks();
-//	int getTotalPrice();
-//	double getPriceAvg();
-//	void sell(String isbn, int quantity);
-//	void buy(String isbn, int quantity);
+	
+	public ArrayList<Book> getBooks() {
+		ArrayList<Book> temp = new ArrayList<>();
+		for(Book b : books) {
+			if(!(b instanceof Magazine)) {
+				temp.add(b);
+			}
+		}
+		return temp;
+	}
+	
+	public ArrayList<Book> getMagazines() {
+		ArrayList<Book> temp = new ArrayList<>();
+		for(Book b : books) {
+			if(b instanceof Magazine) {
+				temp.add(b);
+			}
+		}
+		return temp;
+		
+	}
+	
+	public int getTotalPrice() {
+		int total = 0;
+		for(Book b : books) {
+			total += b.getPrice(); 
+		}
+		return total;
+	}
+
+	public double getPriceAvg() {
+		double avg = getTotalPrice()/books.size();
+		return avg;
+	}
+	
+	public void sell(String isbn, int quantity) throws ISBNNotFoundException,QuantityException {
+		
+		boolean isIn=false;
+		
+		for(Book b:books) {
+			if(b.getIsbn().equals(isbn)) {
+				if(b.getQuantity()-quantity<0) {
+					throw new QuantityException();
+				}
+				b.setQuantity(b.getQuantity()-quantity);
+				isIn=true;
+			}
+		}
+		if(isIn==false) {
+			throw new ISBNNotFoundException();
+		}
+		
+	}
+	public void buy(String isbn, int quantity) throws ISBNNotFoundException{
+		
+		boolean isIn=false;
+		for(Book b:books) {
+			if(b.getIsbn().equals(isbn)) {
+				b.setQuantity(b.getQuantity()+quantity);
+				isIn=true;
+			}
+		} 
+		if(isIn==false) {
+			throw new ISBNNotFoundException();
+		}
+	}
+	
 
 }
--- WSJava03_GUMI06_07th_배혜연/src/com/ssafy/ws07/step3/BookTest.java
+++ WSJava03_GUMI06_07th_배혜연/src/com/ssafy/ws07/step3/BookTest.java
@@ -1,8 +1,85 @@
 package com.ssafy.ws07.step3;
 
+import com.ssafy.ws07.step3.Book;
+import com.ssafy.ws07.step3.Magazine;
+import com.ssafy.ws07.step3.BookManagerImpl;
+
 public class BookTest {
 
 	public static void main(String[] args) {
 		
+//		BookManagerImpl m1 = BookManagerImpl.getInstance();
+//		BookManagerImpl m2 = BookManagerImpl.getInstance();
+//		
+//		if(m1==m2) {
+//			System.out.println("성공");
+//		}
+//		else
+//			System.out.println("실패");
+		
+		BookManagerImpl bm = BookManagerImpl.getInstance();
+		
+		Book book1 = new Book("21424", "Java Pro", "김하나", "jean.kr",15000, "Java 기본문법",5);
+		Book book2 = new Book("21425", "Java Pro2", "김하나", "jean.kr",25000, "Java 응용",10);
+		Book book3 = new Book("35355", "분석설계", "소나무", "jean.kr",30000, "SW 모델링",2);
+		Magazine magazine1 = new Magazine("45678", "월간 알고리즘", "홍길동", "jean.kr",10000, "1월 알고리즘",2021, 1, 3);
+		
+		//도서 추가
+		bm.add(book1);
+		bm.add(book2);
+		bm.add(book3);
+		bm.add(magazine1);
+		
+		//도서전체목록
+		System.out.println("***************************도서 전체 목록***************************");
+		for(Book b:bm.getList()) {
+			System.out.println(b);
+		}
+		//일반도서목록
+		System.out.println("***************************일반 도서 목록***************************");
+		for(Book b:bm.getBooks()) {
+			System.out.println(b);
+		}
+		
+		//잡지목록
+		System.out.println("***************************잡지 목록***************************");
+		for(Book b:bm.getMagazines()) {
+			System.out.println(b);
+		}
+		
+		//도서 제목 포함 검색
+		System.out.println("***************************도서 제목 포함 검색:Java***************************");
+		for(Book b:bm.searchByTitle("Java")) {
+			System.out.println(b);
+		}
+		
+		
+		//도서 가격 총합
+		System.out.println("도서 가격 총합 : "+bm.getTotalPrice());
+		//도서 가격 평균
+		System.out.println("도서 가격 평균 : "+bm.getPriceAvg());
+		
+		//도서 판매
+		System.out.println("***************************도서 판매: 21424, 10권***************************");
+		try {
+			bm.sell("21424",10 );
+			System.out.println("***************************판매 결과***************************");
+			bm.searchByIsbn("21424");
+		}catch(ISBNNotFoundException e){
+			System.out.println(e);
+		}catch(QuantityException e) {
+			System.out.println(e);
+		}
+		
+		//도서 구매
+		System.out.println("***************************도서 구매: 21424, 4권***************************");
+		try {
+			bm.buy("12424", 4);
+			System.out.println("***************************구매 결과***************************");
+			bm.searchByIsbn("12424");
+		}catch(ISBNNotFoundException e){
+			System.out.println(e);
+		}
+				
 	}
 }
