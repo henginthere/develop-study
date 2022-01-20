406b944984b9fd3fa9b4b4f27dcfb191b417ee46
yeonbb98 <yeonbb98@naver.com>
2022-01-20 22:58:13 +0900

    homework04

---
+++ HWJava04_GUMI06_07th_배혜연/src/com/ssafy/hw04/step3/Tv.java
@@ -0,0 +1,32 @@
+package com.ssafy.hw04.step3;
+
+public class Tv extends Product{
+
+	int inch;
+	String type;
+	
+	public Tv(){}
+	public Tv(String pNum, String name, int price, int cnt, int inch, String type){
+		super(pNum, name, price, cnt);
+		this.inch = inch;
+		this.type = type;
+	}
+	
+	public int getInch() {
+		return inch;
+	}
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String toString() {
+		return this.pNum + "| " + this.name + "   |   "+this.price+"   |  "+this.cnt+"     |  "+this.inch+"   |   "+this.type;
+		
+	}
+	
+}
