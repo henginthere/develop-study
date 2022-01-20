48b45b58091407d2046bf95a31d1302d52218bc4
henginthere <hypae@naver.com>
2022-01-21 00:27:18 +0900

    homework04

---
+++ HWJava04_GUMI06_07th_배혜연/src/com/ssafy/hw04/step3/myThread.java
@@ -0,0 +1,58 @@
+package com.ssafy.hw04.step3;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class myThread implements Runnable{
+	
+	public myThread() {}
+
+	@Override
+	public void run() {
+		System.out.println("스레드 실행");
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		
+		try {
+			//fis = new FileInputStream("product.dat");
+			fis = new FileInputStream("product_.dat");
+			ois = new ObjectInputStream(fis);
+
+			ArrayList<Object> tmp = new ArrayList<>();
+		
+			tmp=(ArrayList<Object>) ois.readObject();
+
+			System.out.println("****************************불러온 상품 전체 목록****************************");
+			for(Object o:tmp) {
+				System.out.println(o);
+			}
+		
+		}
+		catch (FileNotFoundException e) {
+			//e.printStackTrace();
+			System.out.println("****************************불러온 상품 전체 목록****************************");
+			System.out.println("등록된 상품이 없습니다.");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				ois.close();
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}catch (NullPointerException e) {
+				
+			}
+		}
+	}
+	
+	
+}
--- WSJava04_GUMI06_07th_배혜연/src/com/ssafy/ws04/step3/myThread.java
+++ WSJava04_GUMI06_07th_배혜연/src/com/ssafy/ws04/step3/myThread.java
@@ -1,5 +1,66 @@
 package com.ssafy.ws04.step3;
 
-public class myThread {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
+public class myThread implements Runnable{
+
+
+	public myThread() {
+		
+	}
+
+	@Override
+	public void run() {
+		
+			FileInputStream fis = null;
+			ObjectInputStream ois = null;
+			System.out.println("스레드실행");
+			try {
+				fis = new FileInputStream("book.dat");
+				//fis = new FileInputStream("book_.dat");
+				ois = new ObjectInputStream(fis);
+
+				ArrayList<Object> tmp = new ArrayList<>();
+			
+				tmp=(ArrayList<Object>) ois.readObject();
+
+				System.out.println("****************************불러온 도서 전체 목록****************************");
+				for(Object o:tmp) {
+					System.out.println(o);
+				}
+			
+			}
+			
+			catch (FileNotFoundException e) {
+				//e.printStackTrace();
+				System.out.println("****************************불러온 도서 전체 목록****************************");
+				System.out.println("등록된 도서가 없습니다.");
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					ois.close();
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}catch (NullPointerException e) {
+					
+				}
+			}
+//			catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+			
+			
+		}
+	
 }
