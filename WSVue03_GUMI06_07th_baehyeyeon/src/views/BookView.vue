2422b7eedec642ae558ae68c918077bf666aaa09
yeonbb98 <yeonbb98@naver.com>
2022-03-15 22:40:00 +0900

    [rename]homework_Vue03_최종

---
+++ WSVue03_GUMI06_07th_baehyeyeon/src/views/BookView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="book">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+
+export default ({
+    name: 'book-view',
+    component:{
+
+    },
+})
+</script>
