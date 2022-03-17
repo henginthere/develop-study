ef745afdc010d3e92213629251d7deb7a38b6ecf
henginthere <hypae@naver.com>
2022-03-18 00:41:08 +0900

    homework_Spring02

---
+++ work_spring/WSSpring02_GUMI06_07th_배혜연/src/main/java/com/ssafy/ws/aop/LoggingAspect.java
@@ -0,0 +1,28 @@
+package com.ssafy.ws.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.aspectj.*;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+	@Pointcut("execution(*(..))")
+	public void pointCut(){}
+	
+	@Before("pointCut()")
+	public void start(JoinPoint joinPoint) {
+		String string = joinPoint.getSignature().getName();
+		System.out.println(string+" 메서드 호출");
+		for(Object arg:joinPoint.getArgs()) {
+			System.out.println(string+ " 아규먼트 "+ arg);
+		}
+	}
+}
