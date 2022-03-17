a418dd8c4e6d8a1af280366d0323c854b856c405
yeonbb98 <yeonbb98@naver.com>
2022-03-18 00:44:38 +0900

    Vue_Day04

---
+++ vue-cli-book-2/src/components/book/BookList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>도서 목록 페이지 입니다.</div>
+</template>
+
+
+<script>
+
+export default{
+    name:'book-list',
+    created(){
+        this.$store.dispatch('getBooks');
+    }
+}
+</script>
---
+++ vue-cli-book-2/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/book',
+    component: () => import('@/views/BookView.vue'),
+    child: [
+      {
+        path: '',
+        name: 'book-list',
+        component:() =>import('@/components/book/BookList.vue')
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
