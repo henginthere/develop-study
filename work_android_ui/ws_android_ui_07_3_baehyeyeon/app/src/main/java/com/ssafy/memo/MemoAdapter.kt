f563ee55f83e59653bdea5e17c0b6682f4ea245b
DESKTOP-KVCQHCD\multicampus <yeonbb98@naver.com>
2022-04-05 16:36:02 +0900

    homework_AOS_UI_07

---
+++ work_android_ui/ws_android_ui_07_3_baehyeyeon/app/src/main/java/com/ssafy/memo/MemoAdapter.kt
@@ -0,0 +1,61 @@
+package com.ssafy.memo
+
+import android.content.Context
+import android.util.Log
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoAdapter(
+    context: Context, val resource: Int, val objects: MutableList<MemoDto>
+) : RecyclerView.Adapter<MemoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(resource, parent, false)
+
+        return MemoViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        // 한 행에 데이터를 넣어준다.
+        holder.tvMemo.text = objects[position].memoTitle + " "+ objects[position].memoDate
+        //Log.d(TAG, "onBindViewHolder: $position 행에 ${objects[position]} 데이터 삽입")
+
+        // OnItemClickListener 연결
+        holder.bindOnItemClickListener(onItemClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return objects.size
+    }
+
+    // 클릭 처리 기능이 내장되어 있지 않아 OnItemClickListener 인터페이스 구현
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+    lateinit var onItemClickListener: OnItemClickListener
+}
+
+class MemoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),View.OnCreateContextMenuListener {
+    var tvMemo = itemView.findViewById(R.id.tv_memo) as TextView
+
+    fun bindOnItemClickListener(onItemClickListener: MemoAdapter.OnItemClickListener) {
+        itemView.setOnClickListener {
+            onItemClickListener.onItemClick(it, adapterPosition)
+        }
+    }
+
+    // Context 메뉴 생성
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) { //v : 사용자가 길게 누른 것
+        menu?.add(0, 0, adapterPosition, "삭제")
+    }
+
+    // Context 메뉴 리스너 등록
+    init {
+        itemView.setOnCreateContextMenuListener(this)
+    }
+
+}
---
+++ work_android_ui/ws_android_ui_07_3_baehyeyeon/app/src/main/java/com/ssafy/memo/MemoEditActivity.kt
@@ -0,0 +1,76 @@
+package com.ssafy.memo
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.ssafy.memo.databinding.ActivityMemoEditBinding
+import com.ssafy.memo.util.Utils
+
+class MemoEditActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMemoEditBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMemoEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val initTodo = intent.getStringExtra("todo")
+        val initDetail = intent.getStringExtra("detail")
+        val initTime = intent.getStringExtra("time")
+        val index = intent.getIntExtra("num",0)
+
+        binding.todo.setText(initTodo)
+        binding.detail.setText(initDetail)
+        binding.editTime.setText(initTime)
+
+        //수정모드일 경우 ( 전달받은 initTodo가 비어있지 않으면)
+        if(initTodo!!.isNotEmpty()){
+            binding.tvTime.visibility = View.VISIBLE
+            binding.editTime.visibility = View.VISIBLE
+            binding.todo.isEnabled = false
+            binding.editTime.isEnabled = false
+        }
+
+        binding.save.setOnClickListener {
+            val newTodo: String = binding.todo.text.toString().trim()
+            val newDetail: String = binding.detail.text.toString().trim()
+            val newTime = Utils.formatter().format(System.currentTimeMillis())
+
+            if (newTodo.isEmpty() || newDetail.isEmpty()) {
+                Toast.makeText(this, "빈 내용이 있습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("inputTodo", newTodo)
+                intent.putExtra("inputDetail", newDetail)
+                intent.putExtra("inputTime",newTime)
+                intent.putExtra("inputNum",index)
+
+                //등록
+                if(initTodo.isEmpty()){
+                    intent.putExtra("state",0)
+                }
+                //수정
+                else{
+                    intent.putExtra("state",1)
+                }
+                //4-1. 결과 전달
+                setResult(Activity.RESULT_OK, intent)
+
+                //3-1. SubActivity 종료
+                finish()
+            }
+        }
+        binding.cancel.setOnClickListener {
+            finish()
+        }
+
+    }
+
+}
+
