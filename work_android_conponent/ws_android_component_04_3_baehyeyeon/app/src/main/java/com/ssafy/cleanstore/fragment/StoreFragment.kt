52bb5f0af9ff74c3a284f4fcae9574c36d783746
DESKTOP-KVCQHCD\multicampus <yeonbb98@naver.com>
2022-04-13 17:49:33 +0900

    homework_AOS_COMPONENT_04

---
+++ work_android_conponent/ws_android_component_04_3_baehyeyeon/app/src/main/java/com/ssafy/cleanstore/fragment/StoreFragment.kt
@@ -0,0 +1,57 @@
+package com.ssafy.cleanstore.fragment
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.cleanstore.databinding.FragmentStoreBinding
+import com.ssafy.cleanstore.stuff.StuffActivity
+
+private const val TAG="StoreFragment_싸피"
+class StoreFragment : Fragment() {
+
+    private lateinit var binding: FragmentStoreBinding
+
+    private lateinit var ctx: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        ctx = context
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentStoreBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.storeInfo1.setOnClickListener {
+            val intent = Intent(ctx, StuffActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach: ")
+    }
+}
