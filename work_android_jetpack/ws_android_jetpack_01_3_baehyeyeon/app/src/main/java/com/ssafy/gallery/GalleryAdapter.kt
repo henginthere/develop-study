555ddccde888ef9443708109eab36a628c298d75
henginthere <hypae@naver.com>
2022-04-27 16:26:51 +0900

    homework_AOS_JETPACK_01_최종

--- work_android_jetpack/ws_android_jetpack_01_3_baehyeyeon/app/src/main/java/com/ssafy/gallery/GalleryAdapter.kt
+++ work_android_jetpack/ws_android_jetpack_01_3_baehyeyeon/app/src/main/java/com/ssafy/gallery/GalleryAdapter.kt
@@ -8,44 +8,54 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.gallery.databinding.ListItemGalleryBinding
 
-class GalleryAdapter(val context: Context, private val photoList: MutableList<Photo>):
+class GalleryAdapter(private val photoList: MutableList<Photo>):
     RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
 
+        //list_item_gallery와 연결되는 ViewHolder
         inner class ViewHolder(private val binding: ListItemGalleryBinding): RecyclerView.ViewHolder(binding.root){
+
+            init{
+                binding.viewModel = PhotoViewModel()
+            }
             fun bind(item: Photo){
                 binding.apply {
-                    // layout의 variable에 Photo 객체 할당
-                    photo = item
+                    // 각 아이템의 Photo 객체를 PhotoViewModel에 연결시킨다.
+                    viewModel?.photo = item
+
+                    // Binding 실행
+                    // 변수 또는 관찰 가능한 객체가 변경될 때 결합은 다음 프레임 이전에 변경되도록 예약됩니다.
+                    // 하지만 결합이 즉시 실행되어야 하는 때도 있습니다.
+                    // 이럴 때 강제로 실행하려면 executePendingBindings() 메서드를 사용하세요.
+                    executePendingBindings()
+
+                    // 클릭 리스너 연결
+                    itemView.setOnClickListener {
+                        itemClickListner.onClick(it, item, adapterPosition)
+                    }
                 }
             }
 
-            fun bindOnItemClickListener(onItemClickListener: GalleryAdapter.OnItemClickListener) {
-                itemView.setOnClickListener {
-                    onItemClickListener.onItemClick(it, adapterPosition)
-                }
-            }
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //list_item_gallery와 연결
         var listItemBinding: ListItemGalleryBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.list_item_gallery, parent, false)
         return ViewHolder(listItemBinding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val photo = photoList[position]
-        holder.apply {
-            bind(photo)
-            itemView.tag = photo
-            bindOnItemClickListener(onItemClickListener)
-        }
+        holder.bind(photo)
     }
 
     override fun getItemCount() = photoList.size
-
-    // 클릭 처리 기능이 내장되어 있지 않아 OnItemClickListener 인터페이스 구현
-    interface OnItemClickListener {
-        fun onItemClick(view: View, position: Int)
+    interface ItemClickListener {
+        fun onClick(view: View, dto:Photo, position: Int)
     }
 
-    lateinit var onItemClickListener: OnItemClickListener
+    private lateinit var itemClickListner: ItemClickListener
+
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListner = itemClickListener
+    }
 }
